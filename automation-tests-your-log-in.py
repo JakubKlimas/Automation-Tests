1748a5a5c882e75053ce91d8b443baa712b524f6
Jakub Klimas <jakub.wincent.klimas@gmail.com>
2021-08-22 23:16:50 +0200

    added functions for this code (code reduction)

--- automation-tests-your-log-in.py
+++ automation-tests-your-log-in.py
@@ -1,128 +1,56 @@
+
 from selenium import webdriver
 import time
 
 web=webdriver.Chrome()
+
 web.get('https://testyourlog.in/example/')
-time.sleep(2)
-
-email = web.find_element_by_xpath('/html/body/div/div/form/div[1]/div[1]/div/input')
-email.send_keys('valid@user.com')
-
-password = web.find_element_by_xpath('/html/body/div/div/form/div[1]/div[2]/div/input')
-password.send_keys('123456')
-
-button = web.find_element_by_xpath('/html/body/div/div/form/div[1]/button')
-button.click()
-get_confirmation_div_text = web.find_element_by_css_selector('.content').text
-if get_confirmation_div_text == "You're logged in!":
-  print('Test completed successfully')
-else:
-  print('Test was not completed successfully')
-  
-web.execute_script('window.history.go(-1)')
-time.sleep(2)
-
-email = web.find_element_by_xpath('/html/body/div/div/form/div[1]/div[1]/div/input')
-email.clear()
-email.send_keys('valid@user.com')
-
-button = web.find_element_by_xpath('/html/body/div/div/form/div[1]/button')
-button.click()
-
-get_confirmation_div_text = web.find_element_by_css_selector('.content').text
-if get_confirmation_div_text == "You're logged in!":
-  print('Test completed successfullyTest was not completed successfully')
-else:
-  print('Test completed successfully')
-  
-time.sleep(2)
-
-email = web.find_element_by_xpath('/html/body/div/div/form/div[1]/div[1]/div/input')
-email.clear()
-email.send_keys('12345')
-
-password = web.find_element_by_xpath('/html/body/div/div/form/div[1]/div[2]/div/input')
-password.clear()
-password.send_keys('1234')
-
-button = web.find_element_by_xpath('/html/body/div/div/form/div[1]/button')
-button.click()
-
-get_confirmation_div_text = web.find_element_by_css_selector('.content').text
-if get_confirmation_div_text == "You're logged in!":
-  print('Test completed successfullyTest was not completed successfully')
-else:
-  print('Test completed successfully')
-  
-time.sleep(2)
-
-email = web.find_element_by_xpath('/html/body/div/div/form/div[1]/div[1]/div/input')
-email.clear()
-
-password = web.find_element_by_xpath('/html/body/div/div/form/div[1]/div[2]/div/input')
-password.clear()
-password.send_keys('123456')
-
-button = web.find_element_by_xpath('/html/body/div/div/form/div[1]/button')
-button.click()
-
-get_confirmation_div_text = web.find_element_by_css_selector('.content').text
-if get_confirmation_div_text == "You're logged in!":
-  print('Test was not completed successfully')
-else:
-  print('Test completed successfully')
 
 time.sleep(2)
 
-email = web.find_element_by_xpath('/html/body/div/div/form/div[1]/div[1]/div/input')
-email.clear()
-email.send_keys('valid@user.com')
+def test1(x, y):
+    email = web.find_element_by_xpath('/html/body/div/div/form/div[1]/div[1]/div/input')
+    email.send_keys(x)
+    password = web.find_element_by_xpath('/html/body/div/div/form/div[1]/div[2]/div/input')
+    password.send_keys(y)  
+    button = web.find_element_by_xpath('/html/body/div/div/form/div[1]/button')
+    button.click()
+    get_confirmation_div_text = web.find_element_by_css_selector('.content').text
+    if get_confirmation_div_text == "You're logged in!":
+        print('Test completed successfully')
+    else:
+        print('Test was not completed successfully')
 
-password = web.find_element_by_xpath('/html/body/div/div/form/div[1]/div[2]/div/input')
-password.clear()
-password.send_keys('1234')
+def test2(x = None , y = None):
+    
+        email = web.find_element_by_xpath('/html/body/div/div/form/div[1]/div[1]/div/input')
+        email.clear()
+        if(x != None):
+            email.send_keys(x)
+     
+        password = web.find_element_by_xpath('/html/body/div/div/form/div[1]/div[2]/div/input')
+        password.clear()
+        if(y != None): 
+            password.send_keys(y)
 
-button = web.find_element_by_xpath('/html/body/div/div/form/div[1]/button')
-button.click()
+        button = web.find_element_by_xpath('/html/body/div/div/form/div[1]/button')
+        button.click()
+        get_confirmation_div_text = web.find_element_by_css_selector('.content').text
+        if get_confirmation_div_text == "You're logged in!":
+            print('Test was not completed successfully')
+        else:
+            print('Test completed successfully')
+        time.sleep(2)
+ 
+test1('valid@user.com', '123456') 
 
-get_confirmation_div_text = web.find_element_by_css_selector('.content').text
-if get_confirmation_div_text == "You're logged in!":
-  print('Test completed successfullyTest was not completed successfully')
-else:
-  print('Test completed successfully')
-  
-time.sleep(2)
+web.execute_script('window.history.go(-1)') 
+time.sleep(2) 
 
-email = web.find_element_by_xpath('/html/body/div/div/form/div[1]/div[1]/div/input')
-email.clear()
-email.send_keys('12345')
+test2(x = 'valid@user.com')
+test2('12345','1234')
+test2(y = '123456')
+test2('valid@user.com','1234')
+test2('1234','123456')
+test2()
 
-password = web.find_element_by_xpath('/html/body/div/div/form/div[1]/div[2]/div/input')
-password.clear()
-password.send_keys('123456')
-
-button = web.find_element_by_xpath('/html/body/div/div/form/div[1]/button')
-button.click()
-
-get_confirmation_div_text = web.find_element_by_css_selector('.content').text
-if get_confirmation_div_text == "You're logged in!":
-  print('Test completed successfullyTest was not completed successfully')
-else:
-  print('Test completed successfully')
-  
-time.sleep(2)
-
-email = web.find_element_by_xpath('/html/body/div/div/form/div[1]/div[1]/div/input')
-email.clear()
-
-password = web.find_element_by_xpath('/html/body/div/div/form/div[1]/div[2]/div/input')
-password.clear()
-
-button = web.find_element_by_xpath('/html/body/div/div/form/div[1]/button')
-button.click()
-
-get_confirmation_div_text = web.find_element_by_css_selector('.content').text
-if get_confirmation_div_text == "You're logged in!":
-  print('Test completed successfullyTest was not completed successfully')
-else:
-  print('Test completed successfully')
