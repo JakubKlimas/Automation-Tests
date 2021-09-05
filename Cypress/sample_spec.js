1090092dde71dbd3f1c62da43ab960c58930787d
Jakub Klimas <jakub.wincent.klimas@gmail.com>
2021-09-06 00:14:23 +0200

    changed code a little bit

--- Cypress/sample_spec.js
+++ Cypress/sample_spec.js
@@ -4,15 +4,15 @@ describe('My first test in cypress', () => {
     it('Visit the Test your login', () =>{
         cy.visit('https://testyourlog.in/example/index.html')
 
-            cy.get(':nth-child(1) > .ui > input')    
+            cy.get('input[placeholder = E-address]')    
                 .type('valid@user.com')
                 .should('have.value','valid@user.com')
 
-                cy.get(':nth-child(2) > .ui > input')    
+                cy.get('input[type = password]')    
                     .type('123456')
                     .should('have.value','123456')
 
-                    cy.get('button').click()
+                    cy.contains('Login').click()
 
                         cy.url().should('include','/logged-in.html?email=valid%40user.com&password=123456')
 
