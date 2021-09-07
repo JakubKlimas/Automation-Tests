3f0b79ea9864b09b59f044bad78280d27e9e53d7
Jakub Klimas <jakub.wincent.klimas@gmail.com>
2021-09-07 18:52:10 +0200

    added new conditions

--- Cypress/sample_spec.js
+++ Cypress/sample_spec.js
@@ -1,7 +1,8 @@
-describe('My first test in cypress', () => {
-    it('Visit the Test your login', () =>{
+context('Actions', () => {
+    beforeEach(() =>{
         cy.visit('https://testyourlog.in/example/index.html')
-
+    })
+    it('Visit the Test your login', () =>{
     cy.get('input[placeholder = E-address]')    
         .type('valid@user.com')
         .should('have.value','valid@user.com')
@@ -10,11 +11,11 @@ describe('My first test in cypress', () => {
         .type('123456')
         .should('have.value','123456')
 
-    cy.contains('Login').click()
+    cy.contains('Login').dblclick()
 
     cy.url().should('include','/logged-in.html?email=valid%40user.com&password=123456')
 
-    cy.contains('Log out').click()
+    cy.contains('Log out').dblclick()
 
     cy.url().should('include','https://testyourlog.in/example/index.html')
                                 
@@ -24,6 +25,10 @@ describe('My first test in cypress', () => {
         .should('have.value','valid@user.com')
 
     cy.contains('Login').click()
+
+    cy.contains('Please enter your password')
+
+    cy.contains('Your password must be at least 6 characters')
 
     cy.url().should('include','https://testyourlog.in/example/index.html')
 
@@ -39,7 +44,16 @@ describe('My first test in cypress', () => {
 
     cy.contains('Login').click()
 
+    cy.contains('Please enter a valid e-mail')
+
+    cy.contains('Your username or password is incorrect')
+
+    cy.contains('Your password must be at least 6 characters')
+
     cy.url().should('include','https://testyourlog.in/example/index.html')
+
+    cy.get('input[placeholder=E-address]')
+    .clear()
 
     cy.get('input[type = password]')
     .clear()    
@@ -47,6 +61,12 @@ describe('My first test in cypress', () => {
         .should('have.value','123456')
 
     cy.contains('Login').click()
+
+    cy.contains('Please enter your e-mail')
+
+    cy.contains('Please enter a valid e-mail')
+
+    cy.contains('Your username or password is incorrect')
 
     cy.url().should('include','https://testyourlog.in/example/index.html')
 
@@ -62,6 +82,8 @@ describe('My first test in cypress', () => {
     
     cy.contains('Login').click()
 
+    cy.contains('Your password must be at least 6 characters')
+
     cy.url().should('include','https://testyourlog.in/example/index.html')
 
     cy.get('input[placeholder = E-address]')
@@ -76,20 +98,31 @@ describe('My first test in cypress', () => {
 
     cy.contains('Login').click()
 
+    cy.contains('Please enter a valid e-mail')
+
+    cy.contains('Your username or password is incorrect')
+
     cy.url().should('include','https://testyourlog.in/example/index.html')
 
     cy.get('input[placeholder = E-address]').invoke('val','')
     .clear()    
             
-
     cy.get('input[type = password]').invoke('val','')
     .clear()    
 
     cy.contains('Login').click()
 
-    cy.url().should('include','https://testyourlog.in/example/index.html')
+    cy.contains('Please enter your e-mail')
 
-               
+    cy.contains('Please enter a valid e-mail')
+
+    cy.contains('Your username or password is incorrect')
+
+    cy.contains('Please enter your password')
+
+    cy.contains('Your password must be at least 6 characters')
+
+    cy.url().should('include','https://testyourlog.in/example/index.html')    
            
     })
 
