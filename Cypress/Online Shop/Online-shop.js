6c5a26b73b77e284018c6c1e1e1bcda0fa47335b
Jakub Klimas <jakub.wincent.klimas@gmail.com>
2021-09-10 13:07:34 +0200

    added new test cases

--- Cypress/Online Shop/Online-shop.js
+++ Cypress/Online Shop/Online-shop.js
@@ -15,6 +15,7 @@ context('Actions', () => {
     })
     
     it('Check scoll response', () => {
+
     cy.scrollTo('bottom', { duration: 2000 })
     
     cy.scrollTo('center', { duration: 2000 })
@@ -25,15 +26,50 @@ context('Actions', () => {
 
     it('Select Dresses category and Casual Dresses subcategory', () => {
 
-    cy.get('.cat-title')
+    cy.get('.cat-title', {duration : 5000})
         .click()
-    
-    cy.get('.menu-mobile-grover').eq(1).click()
+        
+    cy.get('.menu-mobile-grover', {duration : 5000}).eq(1)
+        .click()
 
-    cy.get('[title="Casual Dresses"]').eq(1)
+    cy.get('[title="Casual Dresses"]', {duration : 5000}).eq(1)
         .click()
 
     })
 
+    it('Sort Price: Lowest First', () => {
+
+    cy.get('#selectProductSort').select('price:asc').should('have.value', 'price:asc')
+      
+    })
+
+    it('Select Printed Dress', () => {
+
+    cy.get('.product_img_link')
+        .click()
+
+    })
+
+    it('Select two dresses and add to cart', () => {
+
+    cy.get('[name="qty"]')
+        .click()
+        .clear()
+        .type(2)
+
+    cy.get('#group_1').select('2').should('have.value','2')
+
+    cy.contains('Add to cart')
+        .click({ duration: 2000 })
+        .click({ duration: 2000 })
+
+    })
+
+    it('View the product in cart', () => {
+
+    cy.contains('Proceed to checkout')
+        .click()
+
+    })
   
 })
