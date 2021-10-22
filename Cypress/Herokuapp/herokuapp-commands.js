1703c7535a9ecb93cdb7cc3ffd8c9a662f5fb180
jakub.klimas <jakub.wincent.klimas@gmail.com>
2021-10-22 10:17:13 +0200

    delete old ver

--- Cypress/Herokuapp/herokuapp-commands.js
+++
@@ -1,7 +0,0 @@
-import { HEROKUAPP_SELECTORS } from "../../fixtures/herokuapp-selectors";
-
-Cypress.Commands.add('CheckAllImages', () => {
-cy.get(HEROKUAPP_SELECTORS.IMG_ONE).should('be.visible');
-cy.get(HEROKUAPP_SELECTORS.IMG_TWO).should('be.visible');
-cy.get(HEROKUAPP_SELECTORS.IMG_THREE).should('be.visible');
-});
