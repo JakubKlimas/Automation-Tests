2e0cd0a5f9714b16ca4f05d9fdf3ed6517595ae5
jakub.klimas <jakub.wincent.klimas@gmail.com>
2021-10-26 12:54:24 +0200

    added div to one selector

--- Cypress/Herokuapp/herokuapp_selectors.js
+++ Cypress/Herokuapp/herokuapp_selectors.js
@@ -7,5 +7,5 @@ CONTEXT_MENU: 'div[id = "hot-spot"]',
 COLUMN_A: 'div[id = "column-a"]',
 COLUMN_B: 'div[id = "column-b"]',
 DROPDOWN_LIST: 'select[id = "dropdown"]',
-TEXT_COLUMNS: '[class = "large-10 columns"]',
+TEXT_COLUMNS: 'div[class = "large-10 columns"]',
 };
